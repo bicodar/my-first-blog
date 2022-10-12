f18fe7ad7481d0b74dcea9c9a78d2014a2d58512
bicodar <dario.bicocchi@gmail.com>
2022-10-12 16:39:27 +0200

    lavoro 12/10/2022

--- cv_website/cv_website/urls.py
+++ cv_website/cv_website/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.cvsite, name='home'),
     path('contatti', views.contatti, name='contatti'),
-    path('curriculum',views.curriculum, name='curriculum')  
+    path('curriculum',views.curriculum, name='curriculum'),  
+    path('cv-download/', views.download_file, name='cv-download'),
+    path('detail/<int:cvelement_id>', views.detail, name='detail'),
 ]
+    
--- cv_website/cvsite/views.py
+++ cv_website/cvsite/views.py
@@ -10,3 +10,32 @@ def contatti(request):
     return render(request, 'cvsite/contatti.html')
 def curriculum (request):
     return render(request, 'cvsite/curriculum.html')
+    ...
+
+import mimetypes
+import os
+from django.http.response import HttpResponse
+
+...
+
+def download_file(request):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+		# Modifica filename con il nome e l'estensione del file che vuoi far scaricare	
+    filename = 'largepreview.png'
+    filepath = BASE_DIR + '/download/' + filename
+    path = open(filepath, 'rb')
+    mime_type, _ = mimetypes.guess_type(filepath)
+    response = HttpResponse(path, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
+
+
+from django.http import Http404
+...
+
+def detail(request, cvelement_id):
+    try:
+        print('cv element id: %s' % cvelement_id)
+    except cvelement.DoesNotExist:
+        raise Http404("CV element does not exist")
+    return render(request, 'portfolio/detail.html', {'element': cvelement_id})
